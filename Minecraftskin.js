68c573f6acf50b4e1548b78f61c879c31568de60
SerwenyiW <prokillerfake8@gmail.com>
2024-06-08 11:56:27 +0200

    Jobb

--- Minecraftskin.js
+++ Minecraftskin.js
@@ -31,30 +31,74 @@ function getServer(server) {
     }
 }
 
-function getURLs(username, server) {
-    let link = "";
+async function getURLs(username, server) {
     if(server == undefined) return console.log("Nincs ilyen szerver!"), question();
+    let chooseSkins = [];
+    
     for(let i = 0; i<server.folders.length; i++) {
         if(server.url.includes(".php?")) {
-            fetch(server.account+username, {method: "GET"})
-            .then(response => response.json())
-            .then(data => {
-                link = data.data[server.folders[i]].includes(toString(data.data[server.folders[i-1]]).split("hash=")[1]) ? data.data[server.folders[i]] : null;
-                downloadSkins(link, `${server.folders[i]}_${username}.png`)
-            });
+            try {
+                let response = await fetch(server.account + username);
+                let data = await response.json();
+                let validhash = data.data[server.folders[i]].split("hash=")[1] === "" ? data.data[server.folders[i-1]].split("hash=")[1] : data.data[server.folders[i]].split("hash=")[1];
+                let skinFolders = data.data[server.folders[i]].split(server.url).join("").split("&").join("").split("hash=")[0];
+                chooseSkins.push([`${i + 1}`, skinFolders === "" ? "skin" : skinFolders]);
+                chooseDownload(username, server, chooseSkins, validhash, true, i);
+            } catch (error) {
+                console.error("Hiba történt a fetch kéréssel:", error);
+            }
         } else {
-            link = `${server.url+server.folders[i]}/${username}.png`;
-            downloadSkins(link, `${server.folders[i]}_${username}.png`);
+            chooseSkins.push([`${i + 1}`, server.folders[i]]);
+            chooseDownload(username, server, chooseSkins, "", false, i);
         }
     }
 }
 
+function chooseDownload(username, server, chooseSkins, validhash, php, i) {
+    let foldersName = {
+        "MinecraftBracelet": "Karkötő",
+        "MinecraftTails": "Farok",
+        "MinecraftTopHat": "Kalap",
+        "MinecraftSkins": "Skin",
+        "MinecraftWings": "Szárny",
+        "head": "Fej",
+        "skin": "Skin",
+        "cloakSimple": "Köpeny"
+    };
+
+    if (i === server.folders.length - 1) {
+        console.log(chooseSkins);
+        for (let chI = 0; chI < chooseSkins.length; chI++) {
+            console.log(`${chooseSkins[chI][0]} -> ${foldersName[chooseSkins[chI][1]]}`);
+        }
+        rl.question(`Miket szeretnél letölteni? Válassz 1-${chooseSkins.length}, használd így ha többet akarsz letölteni: 12345, 253, 12, 1 stb. `, (wdownload) => {
+            if (isNaN(wdownload)) {
+                console.log("Nem jó adat!");
+                question();
+                return;
+            }
+            for (let sz = 0; sz < chooseSkins.length; sz++) {
+                if (wdownload.includes(chooseSkins[sz][0])) {
+                    if(php) {
+                        let link = `${server.url + chooseSkins[sz][1].split("skin")}&hash=${validhash}`;
+                        downloadSkins(link, `${chooseSkins[sz][1]}_${username}.png`);
+                    } else {
+                        let link = `${server.url+chooseSkins[sz][1]}/${username}.png`;
+                        downloadSkins(link, `${chooseSkins[sz][1].split("Minecraft")}_${username}.png`);
+                    }
+                }
+            }
+        });
+    }
+}
+
 function downloadSkins(link, filename) {
-    if(link == null) return;
-    link.startsWith("https://") ? https.get(link, resp =>{
-        if(resp.statusMessage == "Not Found") return console.log(`Következőt nem sikerült letölteni: ${link}`);
-        resp.pipe(fs.createWriteStream(filename));}) : "";
-    link.startsWith("http://") ? http.get(link, resp =>{
-        if(resp.statusMessage == "Not Found") return console.log(`Következőt nem sikerült letölteni: ${link}`);
-        resp.pipe(fs.createWriteStream(filename));}) : "";
+    if(link.includes("null")) return console.log("Nincs adat!");
+    (link.startsWith("https://") ? https : http).get(link, resp => {
+        if (resp.statusCode === 404) {
+            console.log(`Következőt nem sikerült letölteni: ${link}`);
+            return;
+        }
+        resp.pipe(fs.createWriteStream(filename));
+    });
 }
